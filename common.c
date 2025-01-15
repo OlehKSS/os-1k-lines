b75c68885a3a983e4a3432c3e0d6139fc002d3ad
Oleh Kozynets <oleh.kozynets@gmail.com>
2025-01-15 22:05:10 +0100

    Create some functions from the C Standard Library.

--- common.c
+++ common.c
@@ -2,6 +2,50 @@
 
 void putchar(char ch);
 
+void *memcpy(void *dst, const void *src, size_t n) {
+    uint8_t *d = (uint8_t *) dst;
+    const uint8_t *s = (const uint8_t *) src;
+    while (n--) {
+        *d++ = *s++;
+    }
+    return dst;
+}
+
+void *memset(void *buf, char c, size_t n) {
+    uint8_t *p = (uint8_t *) buf;
+    while (n--) {
+        *p++ = c; // Assign 'A' to *p, then increment p
+    }
+    return buf;
+}
+
+// TODO: strcpy_s() is the safe version
+char *strcpy(char *dst, const char *src) {
+    char *d = dst;
+    while  (*src) {
+        *d++ = *src++;
+    }
+    *d = '\0';
+    return dst;
+}
+
+// strcmp() returns an integer indicating the result of the
+// comparison, as follows:
+// •  0, if the s1 and s2 are equal;
+// •  a negative value if s1 is less than s2;
+// •  a positive value if s1 is greater than s2.
+int strcmp(const char *s1, const char *s2) {
+    while (*s1 && *s2) {
+        if (*s1 != *s2) {
+            break;
+        }
+        ++s1;
+        ++s2;
+    }
+
+    return *(uint8_t *)s1 - *(uint8_t *)s2;
+}
+
 void printf(const char *fmt, ...) {
     va_list vargs;
     va_start(vargs, fmt);
