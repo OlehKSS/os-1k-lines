b97b84acea233fea56717395bf3a94154909eeb6
Oleh Kozynets <oleh.kozynets@gmail.com>
2025-01-15 22:13:43 +0100

    Create the PANIC macro.

--- kernel.h
+++ kernel.h
@@ -4,3 +4,9 @@ struct sbiret {
     long error;
     long value;
 };
+// The outer loop is used to combine multiple statements into a macro
+#define PANIC(fmt, ...) \
+    do { \
+        printf("PANIC: %s:%d: " fmt "\n", __FILE__, __LINE__, ##__VA_ARGS__); \
+        while (1) {} \
+    } while (0) 
